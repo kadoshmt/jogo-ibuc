e6f5afb5bfca01fd9b255dc90874e8771b30cf54
Janes Roberto <kadoshmt2@gmail.com>
2024-09-21 21:45:43 +0000

    fix-delete-non-existent-user

--- api/src/users/use-cases/delete-user.usecase.ts
+++ api/src/users/use-cases/delete-user.usecase.ts
@@ -1,4 +1,9 @@
-import { Inject, Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+  Inject,
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { UserProfileNotFoundException } from 'src/common/exceptions/user-profile-not-found.exception';
 import { IProfileRepository } from 'src/profile/interfaces/profile-repository.interface';
 import { IUserRepository } from 'src/users/interfaces/user-repository.interface';
@@ -23,25 +28,33 @@ export class DeleteUserUseCase {
     // Verifica se o usuário a der deletado pe o mesmo que receberá os registros
     if (userID === transferibleID) {
       throw new UnauthorizedException(
-        'Transferible User is the same Deleted User',
+        'Transferible User is the same Deleted User.',
       );
     }
 
     // Verifica se o usuário que está tentando executar a ação é um ADMIN
     if (loggedUser.role !== RoleUser.ADMIN) {
-      throw new UnauthorizedException('Logged User is not an ADMIN user');
+      throw new UnauthorizedException('Logged User is not an ADMIN user.');
+    }
+
+    // Verifica se o usuário a ser deletado existe existe
+    const userToBeErased = await this.userRepository.findOneById(userID);
+    if (!userToBeErased) {
+      throw new NotFoundException('User to be deleted not found.');
     }
 
     // Verifica se o transferibleID existe
     const transferibleUser =
       await this.userRepository.findOneById(transferibleID);
     if (!transferibleUser) {
-      throw new UserProfileNotFoundException('Transferible user not fount');
+      throw new UserProfileNotFoundException('Transferible user not fount.');
     }
 
     // verifica se o usuário que receberá os regitros é do tipo ADMIN
     if (transferibleUser.role !== RoleUser.ADMIN) {
-      throw new UnauthorizedException('Transferible User is not an ADMIN user');
+      throw new UnauthorizedException(
+        'Transferible User is not an ADMIN user.',
+      );
     }
 
     // Se houver outras entidades relacionadas, remover aqui
