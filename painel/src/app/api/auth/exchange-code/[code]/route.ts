5fe127889da27da265f805d09bab92445824b9b2
Janes Roberto <kadoshmt2@gmail.com>
2024-09-28 17:24:46 +0000

    Implements signnin with Google

--- painel/src/app/api/auth/exchange-code/[code]/route.ts
+++ painel/src/app/api/auth/exchange-code/[code]/route.ts
@@ -1,26 +1,28 @@
 import { NextResponse } from 'next/server';
 
-export async function POST(request: Request) {
-  const { code } = await request.json();
+export async function GET(request: Request, { params }: { params: { code: string } }) {
+  const { code } = params;
+
+  if (!code) {
+    return NextResponse.json({ message: 'Código não informado' }, { status: 400 });
+  }
 
   try {
     // Chamar a API NestJS para trocar o código pelo JWT
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/exchange-code`, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ code }),
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/exchange-code/${code}`, {
+      method: 'GET',
     });
 
     if (res.ok) {
       const data = await res.json();
-      const { accessToken } = data;
+      const { accessToken, user } = data;
 
       // Definir o cookie HTTP-only com o JWT
       const response = NextResponse.json({ success: true });
       response.cookies.set('token', accessToken, {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
-        sameSite: 'lax',
+        //sameSite: 'lax',
         maxAge: 60 * 60 * 24, // 1 dia
         path: '/',
       });
