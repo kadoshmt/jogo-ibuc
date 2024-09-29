da6d29a1e796ee8fffbbb5fe24b3010ce8e6fa77
Janes Roberto <kadoshmt2@gmail.com>
2024-09-29 20:29:15 +0000

    create inputs form elements

---
+++ painel/src/components/FormElements/InputFile/index.tsx
@@ -0,0 +1,35 @@
+import React, { InputHTMLAttributes } from "react";
+
+interface InputFileProps extends Omit<InputHTMLAttributes<HTMLInputElement>, "type">{
+  customClasses?: string;
+  label?: string;
+  required?: boolean;
+  icon?: React.ReactNode;
+}
+
+const InputFile: React.FC<InputFileProps> = ({
+  customClasses,
+  label,
+  required,
+  icon,
+  ...inputProps
+}) => {
+  return (
+    <>
+      <div className={customClasses}>
+        <label className="mb-3 block text-body-sm font-medium text-dark dark:text-white">
+          {label}
+          {required && <span className="text-red">*</span>}
+        </label>
+
+        <input
+        className="w-full cursor-pointer rounded-[7px] border-[1.5px] border-stroke bg-transparent outline-none transition file:mr-5 file:border-collapse file:cursor-pointer file:border-0 file:border-r file:border-solid file:border-stroke file:bg-[#E2E8F0] file:px-6.5 file:py-[13px] file:text-body-sm file:font-medium file:text-dark-5 file:hover:bg-primary file:hover:bg-opacity-10 focus:border-primary active:border-primary disabled:cursor-default disabled:bg-dark dark:border-dark-3 dark:bg-dark-2 dark:file:border-dark-3 dark:file:bg-white/30 dark:file:text-white dark:focus:border-primary"
+        {...inputProps}
+        type="file"
+        />
+      </div>
+    </>
+  );
+};
+
+export default InputFile;
