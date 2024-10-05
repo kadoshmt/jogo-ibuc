901d02a56634b2e4511903f32b08e128fc9336e4
Janes Roberto <kadoshmt2@gmail.com>
2024-10-05 19:04:37 +0000

    remove console.log

--- painel/src/utils/jwt.ts
+++ painel/src/utils/jwt.ts
@@ -4,9 +4,10 @@ export async function verifyJwt(token: string) {
   try {
     const secret = new TextEncoder().encode(process.env.JWT_SECRET_KEY);
     const { payload } = await jwtVerify(token, secret);
-    return payload;;
+    return payload;
   } catch (error) {
-    console.log("Erro ao verificar JWT:", error);
+    console.log("Erro ao verificar JWT:");
+    //console.log("Erro ao verificar JWT:", error);
     return null;
   }
 }
