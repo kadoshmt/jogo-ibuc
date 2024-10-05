ee1b19f8cf54d58f4fb5cf023fd5884ef423ed70
Janes Roberto <kadoshmt2@gmail.com>
2024-10-05 19:23:04 +0000

    rename type PaginatedOutputDto file

---
+++ painel/src/types/paginatedResponse.ts
@@ -0,0 +1,11 @@
+export type PaginatedOutputDto<T> = {
+  data: T[];
+  meta: {
+    total: number;
+    lastPage: number;
+    currentPage: number;
+    perPage: number;
+    prev: number | null;
+    next: number | null;
+  };
+}
