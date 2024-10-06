d28e9035feee153e659b9d74d86f9972dab4e874
Janes Roberto <kadoshmt2@gmail.com>
2024-10-06 21:48:31 +0000

    Integrate nodemailer to project

--- api/src/shared/shared.module.ts
+++ api/src/shared/shared.module.ts
@@ -2,6 +2,10 @@ import { Module } from '@nestjs/common';
 import { PrismaProfileRepository } from '../profile/repositories/prisma-profile.repository';
 import { PrismaUserRepository } from '../users/repositories/prisma-user.repository';
 import { CheckUsernameIsAvailableUseCase } from 'src/profile/use-cases/check-username-is-available.usecase';
+import { EmailModule } from './email/email.module';
+import { EmailService } from './email/email.service';
+import { GoogleAuthModule } from './google-auth/google-auth.module';
+import { GoogleAuthService } from './google-auth/google-auth.service';
 
 @Module({
   providers: [
@@ -14,11 +18,16 @@ import { CheckUsernameIsAvailableUseCase } from 'src/profile/use-cases/check-use
       useClass: PrismaUserRepository,
     },
     CheckUsernameIsAvailableUseCase,
+    EmailService,
+    GoogleAuthService,
   ],
   exports: [
     'IProfileRepository',
     'IUserRepository',
     CheckUsernameIsAvailableUseCase,
+    EmailService,
+    GoogleAuthService,
   ],
+  imports: [EmailModule, GoogleAuthModule],
 })
 export class SharedModule {}
