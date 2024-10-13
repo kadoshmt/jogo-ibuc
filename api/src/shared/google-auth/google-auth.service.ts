97eb67db06e2af0c754f5b8c9c3e6bd157eca237
Janes Roberto <kadoshmt2@gmail.com>
2024-10-13 19:41:53 +0000

    fix google auth get tokens

--- api/src/shared/google-auth/google-auth.service.ts
+++ api/src/shared/google-auth/google-auth.service.ts
@@ -86,62 +86,65 @@ export class GoogleAuthService {
       throw new Error('Access token not found');
     }
 
-    const REFRESH_TOKEN = decrypt(tokenRecord.refreshToken);
-    let ACCESS_TOKEN = decrypt(tokenRecord.accessToken);
-
-    // const CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
-    // const CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
-    // const REDIRECT_URI =
-    //   process.env.GMAIL_REDIRECT_URI ||
-    //   'http://localhost:3030/auth/gmail/redirect';
+    const refresh_token = decrypt(tokenRecord.refreshToken);
+    let access_token = decrypt(tokenRecord.accessToken);
 
     this.oauth2Client.setCredentials({
-      refresh_token: REFRESH_TOKEN,
-      access_token: ACCESS_TOKEN,
+      refresh_token: refresh_token,
+      access_token: access_token,
     });
 
     // Verifica se o access token expirou
     const isAccessTokenExpired = tokenRecord.expiresAt < new Date();
 
+    console.log('Token expirado? ', isAccessTokenExpired);
+    console.log('Token guardado: ', tokenRecord.expiresAt);
+    console.log('data atual: ', new Date());
+
     if (isAccessTokenExpired) {
       // Obtém um novo access token usando o refresh token
-      const accessTokenResponse = await this.oauth2Client.getAccessToken();
-      const accessToken = accessTokenResponse.token;
+      try {
+        const accessTokenResponse = await this.oauth2Client.getAccessToken();
+        const accessToken = accessTokenResponse.token;
 
-      if (!accessToken) {
-        throw new Error('Failed to create access token');
+        if (!accessToken) {
+          throw new Error('Failed to create access token');
+        }
+
+        // Atualiza o access token e a data de expiração no banco de dados
+        const expiresAt = new Date(Date.now() + 3600 * 1000); // Access token geralmente expira em 1 hora
+
+        await this.googleAuthRepository.createOrUpdateTokens({
+          refreshToken: tokenRecord.refreshToken,
+          accessToken: encrypt(accessToken),
+          expiresAt: expiresAt,
+        });
+
+        // Atualiza o access token em memória
+        access_token = accessToken;
+
+        // Atualiza as credenciais do OAuth2Client
+        this.oauth2Client.setCredentials({
+          refresh_token: refresh_token,
+          access_token: access_token,
+        });
+      } catch (error: any) {
+        console.error('Erro ao obter novo access token:', error);
+
+        // Tratamento específico para erros de token inválido ou expirado
+        if (error.response?.data?.error === 'invalid_grant') {
+          throw new Error(
+            'Refresh token inválido ou expirado. Por favor, reautorize o aplicativo.',
+          );
+        } else {
+          throw new Error('Erro ao atualizar o access token.');
+        }
       }
-
-      // Atualiza o access token e a data de expiração no banco de dados
-      const expiresAt = new Date(Date.now() + 3600 * 1000); // Access token geralmente expira em 1 hora
-
-      await this.googleAuthRepository.createOrUpdateTokens({
-        refreshToken: tokenRecord.refreshToken,
-        accessToken: encrypt(accessToken),
-        expiresAt: expiresAt,
-      });
-
-      // await this.prisma.oAuthToken.update({
-      //   where: { id: tokenRecord.id },
-      //   data: {
-      //     accessToken: encrypt(accessToken),
-      //     expiresAt: expiresAt,
-      //   },
-      // });
-
-      // Atualiza o access token em memória
-      ACCESS_TOKEN = accessToken;
-
-      // Atualiza as credenciais do OAuth2Client
-      this.oauth2Client.setCredentials({
-        refresh_token: REFRESH_TOKEN,
-        access_token: ACCESS_TOKEN,
-      });
     }
 
     return {
-      accessToken: ACCESS_TOKEN,
-      refreshToken: REFRESH_TOKEN,
+      accessToken: access_token,
+      refreshToken: refresh_token,
     };
   }
 }
